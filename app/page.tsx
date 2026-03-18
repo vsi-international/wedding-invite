bbddf459490030337b8ad2d532ba4802fca75562
Saad Abid <vanguardsportsinternational@gmail.com>
2026-03-18 13:16:11 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -48,7 +48,7 @@ function InvitationDetails() {
       <span className="inline-block w-[56px] text-center">D</span>
       <span className="inline-block w-[56px] text-center">N</span>
 
-      <span className="absolute left-1/2 top-[15px] h-12 w-px -translate-x-1/2 bg-[#b7ae87]"></span>
+      <span className="absolute left-1/2 top-[10px] h-12 w-px -translate-x-1/2 bg-[#b7ae87]"></span>
     </div>
   </div>
 
